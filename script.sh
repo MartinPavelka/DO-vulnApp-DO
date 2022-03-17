139efe236fae3ae2636e718a1c703f5e99aa844f
Martin Pavelka <Martypavelka@gmail.com>
2022-03-17 13:59:50 +0100

    stop adding security measures for now

--- script.sh
+++ script.sh
@@ -17,8 +17,9 @@ tar xzf solr-8.11.0.tgz
 bash solr-8.11.0/bin/install_solr_service.sh solr-8.11.0.tgz
 su - solr -c "/opt/solr/bin/solr create -c mytestcollection -n data_driven_schema_configs"
 ## Set authentication - password SolrRocks
-cd /opt/solr/server
-wget https://raw.githubusercontent.com/MartinPavelka/DO-vulnApp-DO/main/security.json
-cp security.json /var/solr/.
-cp security.json /var/solr/data/.
 systemctl restart solr
+# cd /opt/solr/server
+# wget https://raw.githubusercontent.com/MartinPavelka/DO-vulnApp-DO/main/security.json
+# cp security.json /var/solr/.
+# cp security.json /var/solr/data/.
+# systemctl restart solr
