a1f894065ce85771e14fecddffa9b0b77b191b1a
Martin Pavelka <Martypavelka@gmail.com>
2022-03-17 13:49:50 +0100

    get vulnerable java pls

--- script.sh
+++ script.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 ## update repositories
 sudo apt update
-## install openjdk (requirements for solr)
-apt-get install default-jdk -y
+## install java 8u181 (requirements for solr)
+mkdir /usr/java
+cd /usr/java
+wget https://repo.huaweicloud.com/java/jdk/8u181-b13/jdk-8u181-linux-x64.tar.gz
+tar xzvf jdk-8u181-linux-x64.tar.gz
+update-alternatives --install /usr/bin/java java /usr/java/jdk1.8.0_181/bin/java 100
+update-alternatives --install /usr/bin/javac javac /usr/java/jdk1.8.0_05/bin/javac 100
+cd ~/
 ## Download vulnerable Solr 8.11.0
 cd /opt
 wget https://archive.apache.org/dist/lucene/solr/8.11.0/solr-8.11.0.tgz
